69f6c7180c81e694bcc2f70638fc3b29887ad1af
Tais Cristina Frata Bronca <taisbronca@gmail.com>
2024-10-09 08:25:18 -0300

    refactor: change module imports

--- src/database/database.js
+++ src/database/database.js
@@ -1,4 +1,4 @@
-const mongoose = require("mongoose");
+import mongoose from "mongoose";
 
 const connectDatabase = () => {
     console.log("Connecting the database")
@@ -11,4 +11,4 @@ const connectDatabase = () => {
     .catch((error) => console.log(error));
 };
 
-module.exports = connectDatabase;
+export default connectDatabase;
