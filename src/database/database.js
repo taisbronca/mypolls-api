5967b24b283e1b774a4bff5b428b3045075c89b8
Tais Cristina Frata Bronca <taisbronca@gmail.com>
2024-10-10 08:41:55 -0300

    feat: .env and connection with mongodb atlas

--- src/database/database.js
+++ src/database/database.js
@@ -1,12 +1,12 @@
 import mongoose from "mongoose";
 
 const connectDatabase = () => {
-    console.log("Connecting the database")
+  console.log("Connecting the database");
   mongoose
-    .connect(
-      "mongodb+srv://root:root@cluster0.gldvv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
-      { useNewUrlParser: true, useUnifiedTopology: true }
-    )
+    .connect(process.env.MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
     .then(() => console.log("MongoDB Atlas Connected"))
     .catch((error) => console.log(error));
 };
